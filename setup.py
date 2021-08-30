2f78c6b246653823f40d22b146d19a94492f940f
adamewing <adam.ewing@gmail.com>
2021-08-31 09:18:52 +1000

    support --phase in region plots

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='methylartist',
-    version='1.0.5',
+    version='1.0.6',
     author='Adam Ewing',
     author_email='adam.ewing@gmail.com',
     description=("Tools for parsing and plotting nanopore methylation data"),
     license='MIT',
     url='https://github.com/adamewing/methylartist',
-    download_url='https://github.com/adamewing/methylartist/archive/refs/tags/1.0.5.tar.gz',
+    download_url='https://github.com/adamewing/methylartist/archive/refs/tags/1.0.6.tar.gz',
     scripts=['methylartist'],
     packages=find_packages(),
     install_requires = [
@@ -31,6 +31,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
 )
