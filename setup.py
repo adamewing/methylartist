e82eebbfa3cd194777918a405263c028007d63b6
adamewing <adam.ewing@gmail.com>
2025-04-14 23:33:24 +1000

    version bump

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='methylartist',
-    version='1.3.1',
+    version='1.4.0',
     author='Adam Ewing',
     author_email='adam.ewing@gmail.com',
     description=("Tools for parsing and plotting nanopore methylation data"),
     license='MIT',
     url='https://github.com/adamewing/methylartist',
-    download_url='https://github.com/adamewing/methylartist/archive/refs/tags/1.3.1.tar.gz',
+    download_url='https://github.com/adamewing/methylartist/archive/refs/tags/1.4.0.tar.gz',
     scripts=['methylartist'],
     packages=find_packages(),
     install_requires = [
@@ -31,6 +31,9 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 
 )
